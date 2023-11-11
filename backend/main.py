c85e80299aeaa63124dc791a39108894f6c67928
Jacob Feng <peanutChowder@gmail.com>
2023-11-11 16:45:18 -0700

    Added login post route

---
+++ backend/main.py
@@ -0,0 +1,20 @@
+from flask import Flask, request
+from firebase_admin import auth, exceptions
+
+app = Flask(__name__)
+
+
+@app.route('/login', methods=['post'])
+def login():
+    username = request.form['username']
+    password = request.form['password']
+
+    # Check the username and password against Firebase
+    try:
+        user = auth.get_user_by_email(username)
+        if auth.verify_id_token(user.uid, password):
+            return 'Login successful'
+        else:
+            return 'Invalid password'
+    except auth.ErrorInfo:
+        return 'Invalid username'
