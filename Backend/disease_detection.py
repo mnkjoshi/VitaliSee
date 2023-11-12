fc410a9e6cd8c77047ae50d313c81f368a8072ea
rithwikkb <111675689+rithwikkb@users.noreply.github.com>
2023-11-11 22:01:47 -0700

    Change names

--- Backend/disease_detection.py
+++ Backend/disease_detection.py
@@ -8,7 +8,7 @@ import tensorflow as tf
 def load_model():
     global model
     if model is None:
-      model = tf.keras.models.load_model("/3")
+      model = tf.keras.models.load_model("/Model")
       
 
 def read_file_as_image(data) -> np.ndarray:
